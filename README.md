e4ad35b29597ce61a0a1d6a93cdbe83c8679f938
Michael Shafae <mshafae@fullerton.edu>
2025-03-11 22:56:51 -0700

    initial add.

---
+++ README.md
@@ -0,0 +1,3 @@
+# YourIP
+
+A toy application that shows you your IP address. The entire app is in one file.
---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def show_me():
+    ip = request.remote_addr
+    return render_template('show.html', remote_ip=ip)
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name='World'):
+    return render_template('hello.html', name=name.title())
+
+@app.route("/sqrt/<number>")
+def square_root(number="0"):
+    from math import sqrt
+    d = {
+        "number": number,
+        "root": sqrt(float(number))
+    }
+    return render_template('square_root.html', **d)
